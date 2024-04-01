cc6aae13e781216cf526ac03dfe849f316252151
golden-hamster <isack19999@gmail.com>
2024-04-01 18:22:54 +0900

    first commit

---
+++ src/main/java/com/isack/syp/SypApplication.java
@@ -0,0 +1,13 @@
+package com.isack.syp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SypApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SypApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/isack/syp/SypApplicationTests.java
@@ -0,0 +1,13 @@
+package com.isack.syp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SypApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
