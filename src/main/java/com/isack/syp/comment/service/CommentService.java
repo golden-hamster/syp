eda51437441a6d2c6a3bdceccd0795e160c72560
golden-hamster <isack19999@gmail.com>
2024-04-09 13:03:48 +0900

    feat: 댓글 삭제 api 구현 (#21)

--- src/main/java/com/isack/syp/comment/service/CommentService.java
+++ src/main/java/com/isack/syp/comment/service/CommentService.java
@@ -2,6 +2,7 @@ package com.isack.syp.comment.service;
 
 import com.isack.syp.article.domain.Article;
 import com.isack.syp.article.repository.ArticleRepository;
+import com.isack.syp.comment.domain.Comment;
 import com.isack.syp.comment.dto.CommentDto;
 import com.isack.syp.comment.repository.CommentRepository;
 import com.isack.syp.member.domain.Member;
@@ -33,6 +34,16 @@ public class CommentService {
         return commentRepository.save(commentDto.toEntity(article, member)).getId();
     }
 
+    @Transactional
+    public void deleteComment(Long commentId, MemberDto memberDto) {
+        Comment comment = commentRepository.findById(commentId).orElseThrow(IllegalArgumentException::new);// TODO: 따로 처리할 것
+        validateAuthor(memberDto, comment);
+        commentRepository.delete(comment);
+    }
 
-
+    private void validateAuthor(MemberDto memberDto, Comment comment) {
+        if (!comment.isAuthor(memberDto.getId())) {
+            throw new RuntimeException();
+        }
+    }
 }
