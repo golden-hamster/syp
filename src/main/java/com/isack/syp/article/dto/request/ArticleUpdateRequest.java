97b634f72cecfed57a6f488b991405b0462c62f8
golden-hamster <isack19999@gmail.com>
2024-05-25 02:41:20 +0900

    feat: 요청dto 변경 (#55)
    프론트에서 유튜브플레이리스트의 `apiId`를 받는 식에서 `PlaylistItem` 를 프론트에서 여러 개 받는 식으로 변경함.
    
    `thumbnailUrl`첫 번째 아이템의 썸네일임

--- src/main/java/com/isack/syp/article/dto/request/ArticleUpdateRequest.java
+++ src/main/java/com/isack/syp/article/dto/request/ArticleUpdateRequest.java
@@ -1,17 +1,21 @@
 package com.isack.syp.article.dto.request;
 
 import com.isack.syp.article.dto.ArticleDto;
+import com.isack.syp.article.dto.PlaylistItemDto;
 import com.isack.syp.member.dto.MemberDto;
 import com.isack.syp.playlist.dto.PlaylistDto;
 import lombok.Getter;
+
+import java.util.List;
 
 @Getter
 public class ArticleUpdateRequest {
     private String title;
     private String content;
-    private String apiId;
+    private String thumbnailUrl;
+    private List<PlaylistItemDto> playlistItemDtoList;
 
     public ArticleDto toDto(MemberDto memberDto) {
-        return ArticleDto.of(memberDto, title, content, PlaylistDto.of(apiId));
+        return ArticleDto.of(memberDto, title, content, thumbnailUrl, playlistItemDtoList);
     }
 }
