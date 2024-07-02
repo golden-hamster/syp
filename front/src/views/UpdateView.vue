558629611949eb1bd046a259e02c669fef0c03e6
golden-hamster <isack19999@gmail.com>
2024-07-02 19:58:40 +0900

    feat: 게시글 수정화면 구현 (#57)
    게시글 작성화면과 거의 유사함

---
+++ front/src/views/UpdateView.vue
@@ -0,0 +1,170 @@
+<script setup lang="ts">
+import { ref, onMounted, defineProps } from 'vue'
+import { useRoute, useRouter } from 'vue-router'
+import axios from 'axios'
+import { ElMessage } from 'element-plus'
+import PlaylistItem from '@/entity/article/PlaylistItem'
+
+const props = defineProps({
+  articleId: {
+    type: [Number, String],
+    require: true
+  }
+})
+
+const title = ref('')
+const content = ref('')
+const videoUrl = ref('')
+const playlistItems = ref<PlaylistItem[]>([])
+
+const route = useRoute()
+const router = useRouter()
+
+const youtubeAxios = axios.create({
+  withCredentials: false
+})
+
+function extractVideoId(url: string): string | null {
+  const regex =
+    /(?:https?:\/\/)?(?:www\.)?youtube\.com\/.*v=([a-zA-Z0-9_-]+)|(?:https?:\/\/)?(?:www\.)?youtu\.be\/([a-zA-Z0-9_-]+)/
+  const match = url.match(regex)
+  return match ? match[1] || match[2] : null
+}
+
+function addPlaylistItem() {
+  const videoId = extractVideoId(videoUrl.value)
+  if (!videoId) {
+    ElMessage({ type: 'error', message: '유효하지 않은 url 입니다.' })
+    return
+  }
+
+  const isDuplicate = playlistItems.value.some((item) => item.videoId === videoId)
+  if (isDuplicate) {
+    ElMessage({ type: 'error', message: '중복 url 입니다.' })
+    return
+  }
+  youtubeAxios
+    .get(`https://www.googleapis.com/youtube/v3/videos`, {
+      params: {
+        part: 'snippet',
+        id: videoId,
+        key: 'AIzaSyAOGhPJk8EoxaWYwmuQUPUMCnJ_bugn6Xs'
+      }
+    })
+    .then((response) => {
+      const newPlaylistItem = new PlaylistItem({
+        thumbnailUrl: response.data.items[0].snippet.thumbnails.medium.url,
+        videoId: response.data.items[0].id,
+        videoTitle: response.data.items[0].snippet.title
+      })
+      playlistItems.value.push(newPlaylistItem)
+      videoUrl.value = '' // 인풋 필드 초기화
+    })
+}
+
+function deletePlaylistItem(videoId: string) {
+  playlistItems.value = playlistItems.value.filter((item) => item.videoId !== videoId)
+  ElMessage({ type: 'success', message: '플레이리스트 항목이 삭제되었습니다.' })
+}
+
+onMounted(async () => {
+  try {
+    const response = await axios.get(`/api/articles/${props.articleId}`)
+    const article = response.data
+    title.value = article.title
+    content.value = article.content
+    playlistItems.value = article.playlistItemDtoList.map((item: any) => new PlaylistItem(item))
+  } catch (error) {
+    ElMessage({ type: 'error', message: '게시글을 불러오는데 실패했습니다.' })
+  }
+})
+
+const update = () => {
+  const articleId = props.articleId
+  axios
+    .put(`api/articles/${articleId}`, {
+      title: title.value,
+      content: content.value,
+      thumbnailUrl: playlistItems.value[0]?.thumbnailUrl,
+      playlistItemDtoList: playlistItems.value
+    })
+    .then(() => {
+      ElMessage({ type: 'success', message: '게시글이 수정되었습니다.' })
+      router.replace({ name: 'home' })
+    })
+    .catch((error) => {
+      ElMessage({ type: 'error', message: '게시글 수정에 실패했습니다.' })
+    })
+}
+</script>
+
+<template>
+  <div class="container">
+    <el-col :offset="15" :pull="8">
+      <el-input
+        v-model="title"
+        maxlength="30"
+        show-word-limit
+        style="font-size: large"
+        placeholder="제목을 입력해주세요"
+      />
+    </el-col>
+
+    <div class="mt-2">
+      <el-col :offset="15" :pull="8">
+        <el-input
+          v-model="content"
+          type="textarea"
+          rows="2"
+          maxlength="70"
+          show-word-limit
+          style="font-size: large"
+          placeholder="간단하게 내용을 입력해주세요"
+        />
+      </el-col>
+    </div>
+
+    <div class="row mt-5">
+      <div class="col-8 text-end">
+        <el-input
+          v-model="videoUrl"
+          type="text"
+          rows="1"
+          style="width: 28rem"
+          placeholder="플레이리스트에 추가할 영상의 url 을 입력해주세요"
+        />
+      </div>
+
+      <div class="col-4 text-start">
+        <div>
+          <el-button color="#626aef" @click="addPlaylistItem()">영상 추가하기</el-button>
+        </div>
+      </div>
+    </div>
+
+    <div class="row align-items-center" v-for="item in playlistItems" :key="item.videoId">
+      <div class="col-md-4 text-end">
+        <el-image
+          :src="item.thumbnailUrl"
+          :fit="'scale-down'"
+          style="width: 200px; height: 200px"
+        />
+      </div>
+      <div class="col-md-5 text-start">
+        {{ item.videoTitle }}
+      </div>
+      <div class="col-md-3">
+        <el-button color="#626aef" @click="deletePlaylistItem(item.videoId)">X</el-button>
+      </div>
+    </div>
+    <div class="mt-5">
+      <el-col :offset="20" :pull="10">
+        <div class="d-flex justify-content-center">
+          <el-button color="#626aef" @click="update()">플레이리스트 수정하기</el-button>
+        </div>
+      </el-col>
+    </div>
+  </div>
+</template>
+
+<style scoped></style>
